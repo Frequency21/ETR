11d594bd4e7043fc78754f5d24b0f87568dd09eb
Kiss Olivér <h662798@stud.u-szeged.hu>
2020-11-12 00:59:35 +0100

    cleaning up

--- src/main/dao/DAO.java
+++ src/main/dao/DAO.java
@@ -1,8 +1,8 @@
 package main.dao;
 
 public class DAO {
-    String DB_URL = "jdbc:mysql://localhost:3306/adatb";
-    String DRIVER = "com.mysql.cj.jdbc.Driver";
-    String USER = "etr_user";
-    String PASS = "ratards97";
+    final String DB_URL = "jdbc:mysql://localhost:3306/etr";
+    final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    final String USER = "springuser";
+    final String PASS = "ThePassword";
 }
